1c75cc38399b5fd5741977b8b0fbb0966dd10bba
SSM <SA@DESKTOP-NUCU1HD>
2024-09-03 15:04:07 +0500

    Second Commit

--- skinet/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ skinet/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9622b135633072ef160b9b919d3d584377cd159c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+783de0446882226a7f19bcbe63d74c435a6f7c9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
